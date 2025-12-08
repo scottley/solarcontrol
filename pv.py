635b8b147f9bb74bd342e39bbb8b4c33b53645bf
Scott Simon <scott@simon.org>
2025-12-08 14:50:30 -0600

    First code commit

---
+++ pv.py
@@ -0,0 +1,56 @@
+import json
+import code
+import readline
+
+import pyemvue
+
+from pyemvue.enums import Scale, Unit
+
+VUE = pyemvue.PyEmVue()
+
+
+def print_recursive(usage_dict, info, depth=0):
+
+    for gid, device in usage_dict.items():
+        for channelnum, channel in device.channels.items():
+            name = channel.name
+            if name == 'Main':
+                name = info[gid].device_name
+            print('-'*depth, f'{gid} {channelnum} {name} {channel.usage} kwh')
+            if channel.nested_devices:
+                print_recursive(channel.nested_devices, info, depth+1)
+
+def vue_login():
+    with open('emporia_keys.json') as f:
+        data = json.load(f)
+
+    VUE.login(id_token=data['id_token'],
+        access_token=data['access_token'],
+        refresh_token=data['refresh_token'],
+        token_storage_file='emporia_keys.json')
+
+
+vue_login()
+
+devices = VUE.get_devices()
+device_gids = []
+device_info = {}
+for device in devices:
+    if not device.device_gid in device_gids:
+        device_gids.append(device.device_gid)
+        device_info[device.device_gid] = device
+    else:
+        device_info[device.device_gid].channels += device.channels
+
+device_usage_dict = VUE.get_device_list_usage(deviceGids=device_gids, instant=None, scale=Scale.MINUTE.value, unit=Unit.KWH.value)
+print('device_gid channel_num name usage unit')
+print_recursive(device_usage_dict, device_info)
+
+variables = globals().copy()
+variables.update(locals())
+shell = code.InteractiveConsole(variables)
+shell.interact()
+
+
+
+
