b5a66c279a34df9084ab9feafaf79eefc0f50a09
Scott Simon <scott@simon.org>
2025-12-15 14:58:59 -0600

    Talking to Emporia and Hoymiles!

--- pv.py
+++ pv.py
@@ -1,10 +1,13 @@
 import json
 import code
 import readline
+import asyncio
 
 import pyemvue
+import hoymiles_wifi
 
 from pyemvue.enums import Scale, Unit
+from hoymiles_wifi.dtu import DTU
 
 VUE = pyemvue.PyEmVue()
 
@@ -46,10 +49,29 @@ device_usage_dict = VUE.get_device_list_usage(deviceGids=device_gids, instant=No
 print('device_gid channel_num name usage unit')
 print_recursive(device_usage_dict, device_info)
 
+
+dtu = DTU("192.168.1.5")
+
+loop = asyncio.get_event_loop()
+
+#dtu_response = loop.run_until_complete(dtu.async_get_energy_storage_data(430124460077,240424510022))
+dtu_response = loop.run_until_complete(dtu.async_get_gateway_info())
+
+dtu_serial_number = dtu_response.serial_number
+dtu_inverter_serial_number = dtu_response.mdevinfo[0].serial_number
+
+if dtu_response:
+	print(f"DTU Response: {dtu_serial_number} {dtu_inverter_serial_number}")
+else:
+	print("No response from DTU")
+
+loop.close()
+
 variables = globals().copy()
 variables.update(locals())
-shell = code.InteractiveConsole(variables)
-shell.interact()
+
+#shell = code.InteractiveConsole(variables)
+#shell.interact()
 
 
 
